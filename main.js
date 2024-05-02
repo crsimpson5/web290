0da68a26dbffe30d6a984c18a29e75268306a2c4
Curtis Simpson <crsimpson5@gmail.com>
2024-05-02 09:21:10 -0500

    Update form

--- main.js
+++ main.js
@@ -5,24 +5,3 @@ particlesJS.load("particles-js", "assets/particles.json", function () {});
 window.addEventListener("resize", () => {
   particlesJS.load("particles-js", "assets/particles.json", function () {});
 });
-
-// Form submit
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", handleSubmit);
